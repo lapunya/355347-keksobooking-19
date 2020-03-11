ed54ce75cde64e3195f1b8f47bac49d37153bebc
Pavel <pavellapunov@inbox.ru>
2020-03-11 19:47:45 +0300

    fix(backend) - add constats and error listener

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,9 @@
 'use strict';
 
 (function () {
+  var TIMEOUT = 10000;
+  var DOWNLOAD_URL = 'https://js.dump.academy/keksobooking/data';
+  var UPLOAD_URL = 'https://js.dump.academy/keksobooking';
   var code = {
     SUCCESS: 200,
     BAD_REQUEST: 400,
@@ -9,11 +12,10 @@
 
   var download = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
-    var URL = 'https://js.dump.academy/keksobooking/data';
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === code.SUCCESS) {
         onSuccess(xhr.response);
       } else {
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
@@ -28,15 +30,14 @@
       onError('Превышено время ожидания');
     });
 
-    xhr.timeout = 10000;
+    xhr.timeout = TIMEOUT;
 
-    xhr.open('GET', URL);
+    xhr.open('GET', DOWNLOAD_URL);
     xhr.send();
   };
 
   var upload = function (data, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
-    var URL = 'https://js.dump.academy/keksobooking';
 
     xhr.responseType = 'json';
 
@@ -54,7 +55,17 @@
       }
     });
 
-    xhr.open('POST', URL);
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Превышено время ожидания');
+    });
+
+    xhr.timeout = TIMEOUT;
+
+    xhr.open('POST', UPLOAD_URL);
     xhr.send(data);
   };
 
