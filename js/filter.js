500a4ce2c22d2d1e5303e2d4df1f2470f766e452
Pavel <pavellapunov@inbox.ru>
2020-03-16 22:50:00 +0300

    fix(filter) - update filter module

--- js/filter.js
+++ js/filter.js
@@ -1,11 +1,9 @@
 'use strict';
 (function () {
-  var fragment = document.createDocumentFragment();
-  var map = document.querySelector('.map__pins');
-  var maxPinNumber = 5;
-
   var mapFilters = document.querySelector('.map__filters');
   var housingTypeInput = mapFilters.querySelector('#housing-type');
+
+  var maxPinNumber = 5;
 
   var FilterType = {
     type: 'any',
@@ -15,15 +13,13 @@
     fetures: []
   };
 
-  var filteredAdvertisements = []; // массив со всеми объявлениями на сервере
-
   var currentFilterType;
 
   var onSuccessApiResponse = function (advertisements) {
-    filteredAdvertisements = getFilterAdvertisements(advertisements, currentFilterType);
-    window.pin.reset();
-    getRightAmountPins(filteredAdvertisements);
+    var filteredAdvertisements = getFilterAdvertisements(advertisements, currentFilterType);
 
+    window.pin.reset();
+    window.pin.render(filteredAdvertisements);
   };
 
   var onErrorApiResponse = function (errorMessage) {
@@ -32,7 +28,7 @@
 
   var getFilterAdvertisements = function (advertisements, type) {
     if (type === 'any') {
-      return advertisements;
+      return getRightAmountPins(advertisements);
     }
 
     return advertisements.filter(function (item) {
@@ -50,14 +46,12 @@
   housingTypeInput.addEventListener('change', onChangeInput);
 
   var getRightAmountPins = function (data) {
-    data.slice(0, maxPinNumber).forEach(function (elem) {
-      fragment.appendChild(window.pin.create(elem));
-    });
-    map.appendChild(fragment);
+    return data.slice(0, maxPinNumber);
   };
 
   window.filter = {
     getRightAmountPins: getRightAmountPins,
-    advArray: allAdvertisements
+    onErrorApiResponse: onErrorApiResponse,
+    onSuccessApiResponse: onSuccessApiResponse
   };
 })();
