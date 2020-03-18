ba2446980641ec170ccc060cbc668aef24a3ebb2
Pavel <pavellapunov@inbox.ru>
2020-03-18 23:32:11 +0300

    feat(main) - add return of main pin

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@
   var MAIN_PIN_WIDTH = mainPin.offsetWidth;
   var MAIN_PIN_HEIGHT = mainPin.offsetHeight;
 
+  var MAIN_PIN_INITIAL_X = mainPin.style.left;
+  var MAIN_PIN_INITIAL_Y = mainPin.style.top;
+
   var mainPinX = Math.round(parseInt(mainPin.style.left, 10) + MAIN_PIN_WIDTH / 2);
   var inactiveMainPinY = Math.round(parseInt(mainPin.style.top, 10) + MAIN_PIN_HEIGHT / 2);
   var activeMainPinY = Math.round(parseInt(mainPin.style.top, 10) + MAIN_PIN_HEIGHT);
@@ -21,6 +24,8 @@
   var setInactiveState = function () { // неактивное состояние страницы
     adForm.classList.add('ad-form--disabled');
     map.classList.add('map--faded');
+    mainPin.style.left = MAIN_PIN_INITIAL_X;
+    mainPin.style.top = MAIN_PIN_INITIAL_Y;
     for (var i = 0; i < adFormFields.length; i++) {
       adFormFields[i].disabled = true;
     }
